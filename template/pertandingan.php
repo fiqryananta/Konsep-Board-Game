c8503cff73fedf99d937b547fb2898c1acc90cfe
amirmaruf030 <amirmaruf030@gmail.com>
2021-06-23 13:21:05 +0700

    Halaman Pemain

---
+++ template/pertandingan.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Beranda</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="vendors/iconfonts/font-awesome/css/all.min.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.addons.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="images/logo-removebg-preview-1.png" />
+</head>
+
+<body class="horizontal-menu">
+    <div class="container-scroller">
+        <nav class="navbar horizontal-layout-navbar fixed-top navbar-expand-lg">
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+                <a class="navbar-brand brand-logo" href="index.html"><img src="images/logo-removebg-preview-1.png"
+                        alt="logo" /></a>
+                <a class="navbar-brand brand-logo-mini" href="index.html"><img src="images/logo-removebg-preview-1.png"
+                        alt="logo" /></a>
+            </div>
+            <div class="navbar-menu-wrapper d-flex flex-grow">
+                <ul class="navbar-nav navbar-nav-left collapse navbar-collapse" id="horizontal-top-example">
+                    <li class="nav-item">
+                        <a class="nav-link " href="pertandingan.php" aria-expanded="false" style="font-size: medium;">
+                            Pertandingan
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="#" aria-expanded="false" style="font-size: medium;">
+                            Tracking
+                        </a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav navbar-nav-right">
+                    <li class="nav-item nav-profile">
+                        <a href="profile.php" class="nav-link">
+                            <div class="nav-profile-text">
+                                Jane Robert
+                            </div>
+                            <div class="nav-profile-img">
+                                <img src="assets/img/avatar.png" alt="image" class="img-xs rounded-circle ml-3">
+                                <span class="availability-status online"></span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="login.php" class="nav-link">
+                            <i class="fas fa-power-off font-weight-bold icon-sm"></i>
+                        </a>
+                    </li>
+                </ul>
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center ml-auto" type="button"
+                    data-toggle="collapse" data-target="#horizontal-top-example">
+                    <span class="fa fa-bars"></span>
+                </button>
+            </div>
+        </nav>
+        <div class="container-fluid page-body-wrapper">
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <div class="row">
+                        <div class="col-md-12 grid-margin stretch-card">
+                            <div class="card">
+                                <div class="card-body d-flex flex-column">
+                                    <div class="row">
+                                        <div class="col-md-1">
+                                            <img src="assets/img/avatar.png" alt="image"
+                                                class="img-lg rounded-circle d-flex" alt="">
+                                        </div>
+                                        <div class="col-md-1 mr-3"><br>
+                                            <label for="">Ahmad</label><br>
+                                            <label class="badge badge-info badge-pill">Pemain Aktif</label>
+                                        </div>
+                                        <div class="col-md-2 ml-5 mt-3 mr-5">
+                                            <p>
+                                                <i class="icon-sm fas fa-arrow-circle-up mr-2"></i>
+                                                Menang Pertandingan
+                                            </p>
+                                            <h3>345</h3>
+                                        </div>
+                                        <div class="col-md-2 ml-5 mt-3 mr-5">
+                                            <p>
+                                                <i class="icon-sm fas fa-arrow-circle-down mr-2"></i>
+                                                Kalah Pertandingan
+                                            </p>
+                                            <h3>50</h3>
+                                        </div>
+                                        <div class="col-md-3 mt-3">
+                                            <button class="btn btn-success mt-3">Mulai</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="page-header">
+                        <h3 class="page-title">
+                            Data Pertandingan
+                        </h3>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 grid-margin stretch-card">
+                            <div class="card">
+                                <div class="card-body d-flex flex-column">
+                                    <h4 class="card-title">
+                                        <i class="fas fa-calendar"></i>
+                                        Laporan Pertandingan
+                                    </h4>
+
+                                    <div class="table-responsive">
+                                        <table class="table order-listing">
+                                            <thead>
+                                                <tr>
+                                                    <th width="5px">No</th>
+                                                    <th>Nama Pemain</th>
+                                                    <th>Pertandingan</th>
+                                                    <th>Status</th>
+                                                    <th>Terakhir Pertandingan</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Einstin</td>
+                                                    <td>15</td>
+                                                    <th><label class="badge badge-warning badge-pill">Tidak
+                                                            Aktif</label></th>
+                                                    <td>
+                                                        12 Mei 2021
+
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>2</td>
+                                                    <td>Meliodas</td>
+                                                    <td>10</td>
+                                                    <td><label class="badge badge-info badge-pill">Aktif</label></td>
+                                                    <td>
+                                                        17 Mei 2021
+
+
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>3</td>
+                                                    <td>Novita</td>
+                                                    <td>25</td>
+                                                    <td><label class="badge badge-info badge-pill">Aktif</label></td>
+                                                    <td>
+                                                        20 Mei 2021
+
+
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- content-wrapper ends -->
+                <!-- partial:partials/_footer.html -->
+                <footer class="footer">
+                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2020 <a
+                                href="https://www.semogajaya.club/" target="_blank">DOLANAN</a>. All rights
+                            reserved.</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made
+                            with <i class="far fa-heart text-danger"></i></span>
+                    </div>
+                </footer>
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+
+    <!-- plugins:js -->
+    <script src="vendors/js/vendor.bundle.base.js"></script>
+    <script src="vendors/js/vendor.bundle.addons.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="js/off-canvas.js"></script>
+    <script src="js/hoverable-collapse.js"></script>
+    <script src="js/misc.js"></script>
+    <script src="js/settings.js"></script>
+    <script src="js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="js/dashboard.js"></script>
+    <script src="js/data-table.js"></script>
+    <!-- End custom js for this page-->
+</body>
+
+</html>
